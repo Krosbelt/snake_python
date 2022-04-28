235a1b5bed5e750f8c4dc276fad3ce1387796caf
krosbelt <karenvane129@gmail.com>
2022-04-28 14:05:31 -0500

    Detectar colisión con food, bordes y cola, y agregando tabla de puntos

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
-
+from scoreboard import Scoreboard
 import time
 
 # crear escenario
-screen = Screen() #instanciar el objeto
+screen = Screen()  # instanciar el objeto
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Programate snake")
 
-screen.tracer(0) #quitamos animación del escenario
+screen.tracer(0)
 
-#Instanciar (crear) el objeto serpiente
+#instanciar(crear) el objeto serpiente
 snake = Snake()
 
-#Instanciar el objeto food
+#instanciar el objeto food
 food = Food()
 
-#Movimientos serpientes
+#instanciar el objeto tablero de puntos
+Scoreboard = Scoreboard()
+
+#movimientos snake
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
@@ -31,15 +34,25 @@ while game_is_on:
     screen.update()
     time.sleep(0.2)
 
-    #Método de movimiento de la serpiente
     snake.move()
-    # for segment in segments:
-    #     segment.forward(20)
-    #     segment[0].left(90)
 
-    #Detectar colisión con comida
+    #detectar la colisión con la food
     if snake.head.distance(food) < 15:
         food.refresh()
+        Scoreboard.increase_score()
+        snake.extend()
 
-#Final
+    #detectar colisión con los bordes
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        Scoreboard.game_over()  
+
+    #detectar la colision de la cola
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            Scoreboard.game_over()    
+
 screen.exitonclick()
